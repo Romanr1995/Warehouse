8f00aed3d9c5763baee944d80fa7ab31b42470d6
Romanr1995 <roman.nozdracheff@yandex.ru>
2021-12-23 16:05:20 +0300

    Рефакторинг BuyApplication

--- src/main/java/application/BuyApplication.java
+++ src/main/java/application/BuyApplication.java
@@ -1,19 +1,15 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CyclicBarrier;
 
 public class BuyApplication {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         if (args.length == 1) {
             int countBuyers = Integer.parseInt(args[0].trim());
             Warehouse warehouse = new Warehouse();
             CyclicBarrier cyclicBarrier = new CyclicBarrier(countBuyers);
-            List<Buyer> buyers = new ArrayList<>();
             for (int i = 0; i < countBuyers; i++) {
                 Buyer buyer = new Buyer(warehouse, cyclicBarrier);
-                buyers.add(buyer);
                 buyer.start();
             }
 
